5fae5eb5742f4d5d7fff0e72384779b46a11f36f
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2023-12-20 14:26:43 +0100

    add: js validation form

---
+++ js/validations.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const emailInput = document.querySelector('input[type=email]');
+const phoneInput = document.querySelector('input[type=tel]');
+
+const EmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PhoneNumberRegex = /^\+(?:\d{1,3})?\d{10,14}$/;
+
+phoneInput.addEventListener('input', (e)=>{
+    if (PhoneNumberRegex.test(phoneInput.value)) {
+        phoneInput.classList.add('is-valid');
+        phoneInput.classList.remove('is-invalid');
+    } else {
+        phoneInput.classList.add('is-invalid');
+        phoneInput.classList.remove('is-valid');
+    }
+})
+
+emailInput.addEventListener('input', (e)=>{
+    if (EmailRegex.test(emailInput.value)) {
+        emailInput.classList.add('is-valid');
+        emailInput.classList.remove('is-invalid');
+    } else {
+        emailInput.classList.add('is-invalid');
+        emailInput.classList.remove('is-valid');
+    }
+})
+
